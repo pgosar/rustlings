be1542dc09e2877abd6cdac07c20ceada497b8ed
pgosar <pgosarsteam@gmail.com>
2022-06-21 23:45:15 -0500

    move semantics

---
+++ exercises/move_semantics/move_semantics4.rs
@@ -0,0 +1,26 @@
+// move_semantics4.rs
+// Refactor this code so that instead of having `vec0` and creating the vector
+// in `fn main`, we create it within `fn fill_vec` and transfer the
+// freshly created vector from fill_vec to its caller.
+// Execute `rustlings hint move_semantics4` for hints!
+
+fn main() {
+
+    let mut vec0 = fill_vec();
+
+    println!("{} has length {} content `{:?}`", "vec1", vec0.len(), vec0);
+
+    vec0.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec1", vec0.len(), vec0);
+}
+
+// `fill_vec()` no longer takes `vec: Vec<i32>` as argument
+fn fill_vec() -> Vec<i32> {
+    let mut vec1 :Vec<i32> = Vec::new();
+    vec1.push(22);
+    vec1.push(44);
+    vec1.push(66);
+
+    vec1
+}
